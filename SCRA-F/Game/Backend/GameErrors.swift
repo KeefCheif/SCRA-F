bb85caf49b5565f6daa79cca10ab6a37435ec82c
Keef Cheif <Keefcheif.dev@gmail.com>
2022-05-24 10:37:35 -0700

    Game Operations added... Still needs testing

---
+++ SCRA-F/Game/Backend/GameErrors.swift
@@ -0,0 +1,36 @@
+//
+//  GameErrors.swift
+//  SCRA-F
+//
+//  Created by peter allgeier on 5/23/22.
+//
+
+import Foundation
+
+enum GameError: Error, LocalizedError {
+    
+    case createGame
+    case getGame
+    case addPlayer
+    case removePlayer
+    
+    var errorDescription: String? {
+        
+        switch self {
+        case .createGame:
+            return NSLocalizedString("Failed to create the game.", comment: "")
+        case .getGame:
+            return NSLocalizedString("Failed to get game data.", comment: "")
+            
+        case .addPlayer:
+            return NSLocalizedString("Failed to add player to the game.", comment: "")
+        case .removePlayer:
+            return NSLocalizedString("Failed to remove player from the game.", comment: "")
+        }
+    }
+}
+
+struct GameErrorType: Identifiable {
+    let id = UUID()
+    let error: GameError
+}
